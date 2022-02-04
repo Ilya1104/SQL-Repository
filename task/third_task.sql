75bc94d2f73a3ecc76cbeb9101478e1013297deb
ilya kuzhanov <78616695+Ilya1104@users.noreply.github.com>
2022-02-05 00:46:19 +0300

    Delete third_task.sql

--- task/third_task.sql
+++
@@ -1,4 +0,0 @@
-select  acc_id,Account.balance,(Account.balance - sum(Card_account.balance)) as different
-from Account join Card_account on Account.acc_id=Card_account.account_id 
-group by acc_id, Account.balance
-having Account.balance != Account.balance - sum(Card_account.balance)/*õç áë*/
