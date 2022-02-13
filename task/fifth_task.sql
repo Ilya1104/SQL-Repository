0d453f3a720812016ac5f87e364015d10bd34cf2
ilya kuzhanov <78616695+Ilya1104@users.noreply.github.com>
2022-02-13 16:51:07 +0300

    Update fifth_task.sql

--- task/fifth_task.sql
+++ task/fifth_task.sql
@@ -1,6 +1,16 @@
 use db
 go
-create proc add_10_dolars1 as
-update Account set Account.balance = Account.balance+10
-from Account join Social_status on Social_status.status_id=Account.status_id
-where Social_status.status_id=Account.status_id
+create proc add_10_dolars
+@StatusName nvarchar(20)
+as
+if(exists(select Name from SocialStatus where lower(Name)=lower(@StatusName)))
+	begin
+		update Account set Account.balance+=10
+		from Account join SocialStatus on SocialStatus.Id=Account.status_id
+		where Social_status.status_id=Account.StatusId  and SocialStatus.Name=@StatusName
+		print 'Execution is successful !'
+	end
+else
+	begin
+		print 'Status "'+ @StatusName +'" not found'
+	end
