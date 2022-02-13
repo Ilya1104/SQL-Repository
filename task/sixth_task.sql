7c0718ee4683a8407c09972d2b0b00f7bc955dd6
ilya kuzhanov <78616695+Ilya1104@users.noreply.github.com>
2022-02-13 18:00:35 +0300

    Update sixth_task.sql

--- task/sixth_task.sql
+++ task/sixth_task.sql
@@ -1,5 +1,5 @@
 use db
 go
-select Account.Id,  Client.Surname,Client.Name,patronymic, Account.balance - (sum(CardAccount.balance)) as Available_funds
+select Account.Id,  Client.Surname,Client.Name,Client.Patronymic, Account.balance - (sum(CardAccount.balance)) as Available_funds
 from Client join Account on Client.Id=Account.ClientId join CardAccount on Account.Id=CardAccount.AccountId 
 group by Account.Id, Account.balance,Client.Surname,Client.Name,Client.Patronymic
