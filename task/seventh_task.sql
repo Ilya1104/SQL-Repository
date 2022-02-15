8e06ccd775ce446c4972bb7b63f1646bf3338618
ilya kuzhanov <78616695+Ilya1104@users.noreply.github.com>
2022-02-15 17:27:32 +0300

    Update seventh_task.sql

--- task/seventh_task.sql
+++ task/seventh_task.sql
@@ -1,32 +1,24 @@
 use db
 go
-create proc Secure_money_transfer 
+create proc Safe_money_transfer
 @transfer_sum int,
 @number_of_card nvarchar(16)
 as
-	begin try
 begin tran
-		if(exists(select card_number from Card_account where card_number=@number_of_card))
-			begin			
-				select (Account.balance) as [Account balance],
+		if(exists(select Number from CardAccount where Number=@number_of_card))
+			begin		
+			select (Account.balance) as [Account balance],
 				(Account.balance-@transfer_sum) as [Account balance after transfer],
-				(Card_account.balance) as [Card balance before transfer],
-				(Card_account.balance+@transfer_sum) as [Card balance after transfer]
-				from Account join Card_account on Account.acc_id=Card_account.account_id
-				where card_number = @number_of_card 
-				update Card_account set Card_account.balance = Card_account.balance+@transfer_sum
-				from Account join Card_account on Account.acc_id=Card_account.account_id
-				update Account set Account.balance =Account.balance-@transfer_sum
+				(CardAccount.balance) as [Card balance before transfer],
+				(CardAccount.balance+@transfer_sum) as [Card balance after transfer]
+				from Account join CardAccount on Account.Id=CardAccount.AccountId
+				where CardAccount.Number = @number_of_card		
+				update CardAccount set CardAccount.balance +=@transfer_sum where Number=@number_of_card
+				update Account set Account.balance -=@transfer_sum 
+				from Account join CardAccount on Account.Id=CardAccount.AccountId where CardAccount.Number=@number_of_card
 			end
 		else 
-			begin 
+			begin
 				print 'Wrong number of card'
 			end 
-	end try
-	
-	begin catch
-		rollback tran
-		select error_message() as Error
-		return
-	end catch
 commit tran
