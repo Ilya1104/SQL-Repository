21ceb034acf3bff8b0529facb64838ba39ca69ad
ilya kuzhanov <78616695+Ilya1104@users.noreply.github.com>
2022-02-15 18:03:37 +0300

    Update first_task.sql

--- task/first_task.sql
+++ task/first_task.sql
@@ -2,4 +2,4 @@ select Bank.Name,City.Name, Filial.Adress
 from Bank 
 join Filial on Bank.Id=Filial.Id 
 join City on Filial.CityId=City.CityId 
-where City.Name = 'Ìèíñê'
+where City.Name = N'Минск'
