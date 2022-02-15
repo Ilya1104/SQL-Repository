f4a044e2275b5a35ecf824adbc96a556dc587bfc
ilya kuzhanov <78616695+Ilya1104@users.noreply.github.com>
2022-02-15 18:05:30 +0300

    Update second_task.sql

--- task/second_task.sql
+++ task/second_task.sql
@@ -1,3 +1,3 @@
-select Card_account.Number, CardAccount.balance, Client.Surname,Client.Name, Patronymic,Bank.Name, Adress
+select CardAccount.Number, CardAccount.balance, Client.Surname,Client.Name, Patronymic,Bank.Name, Adress
 from Client,Account,Filial,Bank,CardAccount
 where Bank.Id=Filial.BankId and Filial.Id=Account.FilialId and Account.ClientId =Client.Id and Account.Id=CardAccount.AccountId
