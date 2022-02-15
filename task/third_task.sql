7ac859ea2f9e81e55f49e165c68d5891457ca435
ilya kuzhanov <78616695+Ilya1104@users.noreply.github.com>
2022-02-15 18:10:40 +0300

    Update third_task.sql

--- task/third_task.sql
+++ task/third_task.sql
@@ -1,4 +1,4 @@
 select  Account.Id,Account.balance,(Account.balance - sum(CardAccount.balance)) as different
 from Account join CardAccount on Account.Id=CardAccount.AccountId 
 group by Account.Id, Account.balance
-having Account.balance != Account.balance - sum(Card_account.balance)
+having Account.balance != Account.balance - sum(CardAccount.balance)
