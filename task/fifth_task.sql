77e47236e41be92b1a625b76a6db83ae7f6e26e5
ilya kuzhanov <78616695+Ilya1104@users.noreply.github.com>
2022-02-21 15:50:43 +0300

    Update fifth_task.sql

--- task/fifth_task.sql
+++ task/fifth_task.sql
@@ -5,12 +5,19 @@ create proc add_10_dolars
 as
 if(exists(select Name from SocialStatus where lower(Name)=lower(@StatusName)))
 	begin
+		select Client.Name, Patronymic, Surname, SocialStatus.Name, (balance) as [Sum before transfer], (balance+10) as [Sum after transfer] 
+		from Client join Account on Client.Id=Account.ClientId join SocialStatus on Account.StatusId = SocialStatus.Id
+		where SocialStatus.Name=@StatusName
 		update Account set Account.balance+=10
-		from Account join SocialStatus on SocialStatus.Id=Account.status_id
-		where Social_status.status_id=Account.StatusId  and SocialStatus.Name=@StatusName
+		from Account join SocialStatus on SocialStatus.Id=Account.StatusId
+		where SocialStatus.Id=Account.StatusId and SocialStatus.Name=@StatusName
 		print 'Execution is successful !'
 	end
 else
 	begin
 		print 'Status "'+ @StatusName +'" not found'
 	end
+
+exec add_10_dolars @StatusName = 'пенсионер'
+exec add_10_dolars @StatusName = 'РабоТАЮщий'
+exec add_10_dolars @StatusName = 'твмлытмл'
